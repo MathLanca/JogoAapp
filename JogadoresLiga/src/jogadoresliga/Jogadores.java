87fdeacb523730d82aeaf55b8d69366f9efc45a7
Matheus Lança <matheus.7910@hotmail.com>
2018-05-09 20:24:40 -0300

    Implementando a classe Time.

--- JogadoresLiga/src/jogadoresliga/Jogadores.java
+++ JogadoresLiga/src/jogadoresliga/Jogadores.java
@@ -12,31 +12,31 @@ public class Jogadores {
     public static void main(String[] args) {
         LeitorDadosJogadores leitor = new LeitorDadosJogadores();
         String[] dados = leitor.ler();
-        List<Confederacao>confederacoes = new ArrayList();
-        
+        List<Confederacao> confederacoes = new ArrayList();
+
         for (String dado : dados) {
-            String [] registro = dado.split(";");
-            
+            String[] registro = dado.split(";");
+
             Confederacao confederacao = new Confederacao(registro[0]);
-            if(!confederacoes.contains(confederacao)){
+            if (!confederacoes.contains(confederacao)) {
                 confederacoes.add(confederacao);
-                    }
-            
+            }
+
             Liga liga = new Liga(registro[1]);
             int indexOfConfedaracao = confederacoes.indexOf(confederacao);
-            if(!confederacoes.get(indexOfConfedaracao).getLigas().contains(liga)){
+            if (!confederacoes.get(indexOfConfedaracao).getLigas().contains(liga)) {
                 confederacoes.get(indexOfConfedaracao).addLiga(liga);
             }
-    }
-        System.out.println(confederacoes.size());  
+        }
+        System.out.println(confederacoes.size());
         Collections.sort(confederacoes);
-        for (Confederacao conf: confederacoes){
+        for (Confederacao conf : confederacoes) {
             System.out.println(conf.getNome());
             Collections.sort(conf.getLigas());
-            for(Liga liga: conf.getLigas()){
-                System.out.println("\t"+liga.getNome());
+            for (Liga liga : conf.getLigas()) {
+                System.out.println("\t" + liga.getNome());
 
             }
+        }
     }
-}
 }
