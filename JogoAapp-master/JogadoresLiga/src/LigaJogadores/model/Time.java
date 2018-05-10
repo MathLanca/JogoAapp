00bbeeb9b0b0faff2bb68db4eb16b8655019b611
GabrielDiasMarinho <39140131+GabrielDiasMarinho@users.noreply.github.com>
2018-05-09 21:02:55 -0300

    Version 1.0

---
+++ JogoAapp-master/JogadoresLiga/src/LigaJogadores/model/Time.java
@@ -0,0 +1,73 @@
+package LigaJogadores.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Time implements Serializable,Comparable<Time>{
+    private String nome;
+    private List<Jogador> jogadores = new ArrayList();
+    
+    public Time(){
+        
+    }
+    
+    public Time(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Jogador> getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(List<Jogador> jogadores) {
+        this.jogadores = jogadores;
+    }
+    
+    public void addJogador(Jogador jogador){
+        this.jogadores.add(jogador);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        hash = 97 * hash + Objects.hashCode(this.jogadores);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Time other = (Time) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public int compareTo(Time time) {
+        return this.nome.compareTo(time.getNome());
+    }
+    
+    
+}
