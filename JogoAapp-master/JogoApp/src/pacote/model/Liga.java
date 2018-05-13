dc9876a5773f0d59c796f41df6f4cf6b365531f5
Matheus Lança <matheus.7910@hotmail.com>
2018-05-13 19:14:01 -0300

    Descompactando pasta do giazzi

---
+++ JogoAapp-master/JogoApp/src/pacote/model/Liga.java
@@ -0,0 +1,115 @@
+package pacote.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Liga implements Serializable, Comparable<Liga> {
+    private String nome;
+    public List<Time> times = new ArrayList();
+    public Time[] times2;
+    public int numTimes;
+    public Jogo[] jogos;
+    //public List<Jogo> jogos = new ArrayList();
+    
+    public Liga(){
+        
+    }
+    
+    public Liga(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public List<Time> getTimes() {
+        return times;
+    }
+
+    public void setTimes(List<Time> times) {
+        this.times = times;
+    }
+    
+    public void addTime(Time time){
+        this.times.add(time);
+    }
+    
+    /*public int getNumTimes(){
+        this.numTimes = times.size();
+        return this.numTimes;
+    }*/
+    
+    public void organizarJogos(){
+        int numJogo = 0;
+        this.numTimes = times.size();
+        //System.out.println(numTimes);
+        if (numTimes <= 10){
+            /*try{
+                for (int a = 0; a < numTimes; a++){
+                    System.out.println(times.get(a).getNome() + " X " + times.get(a+1).getNome());
+                    //System.out.println(times.get(a+1).getNome() + " X " + times.get(a).getNome());
+                }
+            } catch(IndexOutOfBoundsException ex){
+                System.out.println("");
+            }*/
+            
+            // Vamos ver essa parte do Jogo
+            for(int t1 = 0; t1 < numTimes - 1; t1++){
+                for(int t2 = t1; t2 < numTimes; t2++){
+                    this.jogos[numJogo] = new Jogo(times2[t1],times2[t2]);
+                    numJogo += 1;
+                }
+            }
+        } else{
+            /*
+            for(Time t1 : times){
+                for(Time t2 : times){
+                    System.out.println(t1.getNome() + " X " + t2.getNome());
+                }
+            }*/
+            System.out.println("aaa");
+          }
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.nome);
+        hash = 79 * hash + Objects.hashCode(this.times);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Liga other = (Liga) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    @Override
+    public int compareTo(Liga liga) {
+      return this.nome.compareTo(liga.getNome());
+    }
+    
+    
+}
---
+++ JogoAapp-master/JogoApp/src/pacote/util/Leitor.java
@@ -0,0 +1,78 @@
+package pacote.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import jogadoresliga.LeitorDadosJogadores;
+import pacote.model.Confederacao;
+import pacote.model.Jogador;
+import pacote.model.Liga;
+import pacote.model.Time;
+import pacote.model.Jogo;
+
+public class Leitor {
+    public static void main(String[] args){
+        LeitorDadosJogadores leitor = new LeitorDadosJogadores();
+        String [] dados = leitor.ler();
+        List<Confederacao> confederacoes = new ArrayList();
+        
+        //carregadar os dados
+        for (String dado : dados){
+            String [] registro = dado.split(";");
+            
+            //adicionando a confederação
+            Confederacao confederacao = new Confederacao(registro[0]);
+            if(!confederacoes.contains(confederacao)){
+                confederacoes.add(confederacao);
+            }
+            
+            //adicionando ligas na confederação
+            Liga liga = new Liga(registro[1]);
+            int indexOfConfederacao = confederacoes.indexOf(confederacao);
+            if(!confederacoes.get(indexOfConfederacao).getLigas().contains(liga)){
+                confederacoes.get(indexOfConfederacao).addLiga(liga);
+            }
+            
+            //adicionando times nas ligas
+            Time time = new Time(registro[2]);
+            int indexOfLiga = confederacoes.get(indexOfConfederacao).ligas.indexOf(liga);
+            if(!confederacoes.get(indexOfConfederacao).ligas.get(indexOfLiga).getTimes().contains(time)){
+                confederacoes.get(indexOfConfederacao).ligas.get(indexOfLiga).addTime(time);
+            }
+            
+            //adicionando jogadores nos times
+            Jogador jogador = new Jogador(registro[3], registro[4], registro[5]);
+            int indexOfTimes = confederacoes.get(indexOfConfederacao).ligas.get(indexOfLiga).times.indexOf(time);
+            if(!confederacoes.get(indexOfConfederacao).ligas.get(indexOfLiga).times.get(indexOfTimes).getJogadores().contains(jogador)){
+                confederacoes.get(indexOfConfederacao).ligas.get(indexOfLiga).times.get(indexOfTimes).addJogador(jogador);
+            
+            }
+            
+            liga.organizarJogos();
+            Jogo jogo = new Jogo();
+            System.out.println(jogo.getJogo());
+            
+        }
+        
+        
+        //ler os dados 
+        /*Collections.sort(confederacoes);
+        for (Confederacao conf:confederacoes){
+            System.out.println(conf.getNome());
+            Collections.sort(conf.getLigas());
+            for(Liga l : conf.getLigas()){
+                System.out.println("\t" + l.getNome());
+                for(Time t : l.getTimes()){
+                    System.out.println("\t\t" + t.getNome());
+                    for(Jogador jogador : t.getJogadores()){
+                        System.out.println("\t\t\t" + jogador.getNome() + ", " + jogador.getDataNasc() + ", " + jogador.getLocalNasc());
+                    }
+                }
+            }
+        }*/
+            
+    }
+       
+}
+
+
